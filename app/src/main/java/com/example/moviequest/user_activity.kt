431afa1141e621936e8ab6fe471251a9b6bb5fc4
Yago Garcia <yagog5002@gmail.com>
2024-12-06 18:53:02 +0100

    users in progress

--- app/src/main/java/com/example/moviequest/user_activity.kt
+++ app/src/main/java/com/example/moviequest/user_activity.kt
@@ -11,10 +11,5 @@ class user_activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_user)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
