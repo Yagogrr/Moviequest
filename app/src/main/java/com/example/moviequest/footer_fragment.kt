0f7f24fd110e90d3760aa3a936b2819678af3c1a
Yago Garcia <yagog5002@gmail.com>
2024-12-06 20:43:48 +0100

    footer fixed

--- app/src/main/java/com/example/moviequest/footer_fragment.kt
+++ app/src/main/java/com/example/moviequest/footer_fragment.kt
@@ -8,16 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [footer_fragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class footer_fragment : Fragment() {
 
     override fun onCreateView(
@@ -26,23 +16,22 @@ class footer_fragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_footer_fragment, container, false)
 
-        // Referencias a los ImageView
+        // ImageView references
         val controller: ImageView = view.findViewById(R.id.controller)
         val party: ImageView = view.findViewById(R.id.party)
         val house: ImageView = view.findViewById(R.id.house)
         val search: ImageView = view.findViewById(R.id.search)
         val user: ImageView = view.findViewById(R.id.user)
 
-
+        // Intent to buscar_peliculas
         search.setOnClickListener {
-            // Acción para la búsqueda
-            val intent = Intent(activity, footer_fragment::class.java)
+            val intent = Intent(requireActivity(), buscar_peliculas::class.java)
             startActivity(intent)
         }
 
+        // Intent to user_activity
         user.setOnClickListener {
-            // Acción para el usuario
-            val intent = Intent(activity, user_activity::class.java)
+            val intent = Intent(requireActivity(), user_activity::class.java)
             startActivity(intent)
         }
 
