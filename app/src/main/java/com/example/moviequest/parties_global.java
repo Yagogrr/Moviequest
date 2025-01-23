4a793cbf9651cdb8b3b90090948694d9abbd1fc7
Ariadna <ariadnitaa24@gmail.com>
2025-01-23 16:48:28 +0100

    a

--- app/src/main/java/com/example/moviequest/parties_global.java
+++ app/src/main/java/com/example/moviequest/parties_global.java
@@ -1,6 +1,7 @@
 package com.example.moviequest;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +10,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class parties_global extends AppCompatActivity {
-
+    Button btn_menu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_parties_global);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        btn_menu = findViewById(R.id.btn_desplegable);
+        registerForContextMenu(btn_menu);
+
     }
 }
