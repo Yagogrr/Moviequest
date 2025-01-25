7dde1949019565263371672731bc474a80f7a7b7
Yago Garcia <yagog5002@gmail.com>
2025-01-25 13:47:15 +0100

    categorias menu desplegable

--- app/src/main/java/com/example/moviequest/MovieProvider.kt
+++ app/src/main/java/com/example/moviequest/MovieProvider.kt
@@ -3,7 +3,7 @@ package com.example.moviequest
 class MovieProvider {
     companion object {
         var movieList = listOf<Movie>(
-            Movie("Cadena Perpetua", "Thriller", R.drawable.shawshank_redemption),
+            Movie("Cadena Perpetua", "Action", R.drawable.shawshank_redemption),
             Movie("From Dusk Until Dawn", "Action", R.drawable.abierto_hasta_el_amanecer),
             Movie("Fight Club", "Mistery", R.drawable.fight_club),
             Movie("Shutter Island", "Thriller", R.drawable.shutter_island),
