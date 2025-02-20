06c871a27d1708a25e12fa8204ecd580558e0517
Yago Garcia <yagog5002@gmail.com>
2025-02-20 17:29:41 +0100

    Formulario crear partie

--- app/src/main/java/com/example/moviequest/Partiesglobal.kt
+++ app/src/main/java/com/example/moviequest/Partiesglobal.kt
@@ -2,14 +2,17 @@ package com.example.moviequest;
 
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.LayoutInflater
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText
 import android.widget.PopupMenu
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -31,7 +34,7 @@ class Partiesglobal : AppCompatActivity() {
 
                 when(menuItem.itemId){
                     R.id.part_create ->{
-                        Toast.makeText(this@Partiesglobal, "Crear partie", Toast.LENGTH_LONG).show()
+                        mostrarPopupFormulario()
                         true
                     }
 
@@ -47,5 +50,32 @@ class Partiesglobal : AppCompatActivity() {
             popupMenu.show();
 
         }
+    }
+
+    private fun mostrarPopupFormulario() {
+        val builder = AlertDialog.Builder(this)
+        val inflater = LayoutInflater.from(this) // o getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val dialogView = inflater.inflate(R.layout.popup_formulario, null)
+        builder.setView(dialogView)
+
+        val editTextNombre = dialogView.findViewById<EditText>(R.id.editTextTitol)
+        val editTextDescripcio = dialogView.findViewById<EditText>(R.id.editTextDescripcio)
+
+        builder.setTitle("Formulario de Partie")
+        builder.setPositiveButton("Enviar") { dialog, which ->
+            val nombre = editTextNombre.text.toString()
+            val email = editTextDescripcio.text.toString()
+
+            val mensaje = "Nombre: $nombre, Email: $email"
+            Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
+
+            dialog.dismiss()
+        }
+        builder.setNegativeButton("Cancelar") { dialog, which ->
+            dialog.dismiss()
+        }
+
+        val dialog = builder.create()
+        dialog.show()
     }
 }
